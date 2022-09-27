98e6f729e7a796c5a87bef062c6995698d40263e
MergoldShey <soronnadishiphiah@gmail.com>
2022-09-27 02:49:19 +0000

    A function in C that checks if a singly linked list is a palindrome

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,102 +1,33 @@
 #include "lists.h"
-#include <stdio.h>
-#include <stdlib.h>
-
-int compare(listint_t *head, listint_t *head2);
-listint_t *reverse_list(listint_t *head);
-listint_t *duplicate_list(listint_t *head);
 
 /**
-  * duplicate_list - Duplicates a list
-  * @head: list to be puplicated
-  * Return: duplicated list
-  */
-listint_t *duplicate_list(listint_t *head)
-{
-	listint_t *h = head, *dup = NULL, *tail = NULL;
-
-	while (h != NULL)
-	{
-		if (dup == NULL)
-		{
-			dup = malloc(sizeof(listint_t));
-			dup->n = h->n;
-			dup->next = NULL;
-			tail = dup;
-		}
-		else
-		{
-			tail->next = malloc(sizeof(listint_t));
-			tail = tail->next;
-			tail->n = h->n;
-			tail->next = NULL;
-		}
-		h = h->next;
-	}
-
-	return (dup);
-}
-/**
-  * reverse_list - reverse a list
-  * @head: list to be reversed
-  * Return: reversed list
-  */
-listint_t *reverse_list(listint_t *head)
-{
-	listint_t *current, *prev = NULL, *next = NULL;
-
-	current = head;
-	while (current != NULL)
-	{
-		next = current->next;
-		current->next = prev;
-		prev = current;
-		current = next;
-	}
-
-	return (prev);
-}
-/**
-  * compare - compares two lists
-  * @head: 1st list
-  * @head2: 2nd list
-  * Return: 0 or 1
-  */
-int compare(listint_t *head, listint_t *head2)
-{
-	listint_t *h;
-
-	h = head;
-	while (h != NULL && head2 != NULL)
-	{
-		if (h->n != head2->n)
-			return (0);
-
-		h = h->next;
-		head2 = head2->next;
-	}
-	if (h == NULL && head2 == NULL)
-		return (1);
-	else
-		return (0);
-}
-/**
-  * is_palindrome - checks if a list is a palindrome
-  * @head: list
-  * Return: 0 or 1
-  */
+ * is_palindrome - check for palindrome linkedlist
+ * @head: linkedlist
+ * Return: 1 if palindrome, 0 otherwise
+ */
 int is_palindrome(listint_t **head)
 {
-	listint_t *head2, *h;
-	int res;
+	listint_t *cp = *head;
+	int buff[10240], init = 0, end = 0;
 
-	h = *head;
-	if (h == NULL)
+	if (!*head || !((*head)->next))
 		return (1);
-	head2 = duplicate_list(h);
-	head2 = reverse_list(head2);
-	res = compare((*head), head2);
-	free_listint(head2);
 
-	return (res);
+	while (cp)
+	{
+		buff[end] = cp->n;
+		cp = cp->next;
+		end++;
+	}
+
+	end--;
+
+	while (init <= end / 2)
+	{
+		if (buff[init] != buff[end - init])
+			return (0);
+		init++;
+	}
+
+	return (1);
 }
